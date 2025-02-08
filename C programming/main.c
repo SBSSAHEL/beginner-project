38193974dc6f16baa0af1916a7128a1f8894a60a
SBSSAHEL <sahlshanbin@gmail.com>
2025-02-08 20:22:23 +0600

    C progrmming

---
+++ C programming/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    
+    int a = 20 ;
+    float CGPA =  4.29;
+    double bonus = 3333.33333;
+    char name[] = "sahel";
+    bool single = true ;
+    char sec = 'A';
+
+    printf("A = %d\n",a);
+    printf("GRADE = %f\n",CGPA);
+    printf("bonus = %lf\n",bonus);
+    printf("name = %s\n", name);
+    printf("single: %d\n",single);
+    printf("Sec= %c",sec);
+
+    return 0;
+}
